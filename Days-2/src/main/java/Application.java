e62321f597ce80eb7d8f126b66ddf93857cca5bd
Hakan ÖZER <hakanozer02@gmail.com>
2023-05-30 11:14:25 +0300

    Fİnal

---
+++ Days-2/src/main/java/Application.java
@@ -0,0 +1,11 @@
+import utils.Config;
+
+public class Application {
+    public static void main(String[] args) {
+
+        Config config = new Config();
+        System.out.println( config.appTitle );
+
+
+    }
+}
---
+++ Days-2/src/main/java/utils/Config.java
@@ -0,0 +1,7 @@
+package utils;
+
+public class Config {
+
+    public final String appTitle = "App Title";
+
+}
