2c065c7223b5500834057e03db6c45d65d42b2fe
Hakan ÖZER <hakanozer02@gmail.com>
2023-05-30 11:48:13 +0300

    Static

--- Days-2/src/main/java/Application.java
+++ Days-2/src/main/java/Application.java
@@ -3,9 +3,17 @@ import utils.Config;
 public class Application {
     public static void main(String[] args) {
 
-        Config config = new Config();
-        System.out.println( config.appTitle );
+        Config obj = new Config();
+        System.out.println( obj.appTitle );
 
+        Config.name = "Erkan Bilsin";
+        System.out.println(Config.name);
+
+        int rnd = Config.random.nextInt(100);
+        System.out.println(rnd);
+
+        int sm = Config.sum(454,767);
+        System.out.println("Sm :" + sm);
 
     }
 }
--- Days-2/src/main/java/utils/Config.java
+++ Days-2/src/main/java/utils/Config.java
@@ -1,7 +1,24 @@
 package utils;
 
+import java.util.Random;
+
 public class Config {
 
     public final String appTitle = "App Title";
+
+    // static kullanımı
+    // Sınıfa ait olmak zorundadır.
+    // Erişim belirteçleri alırlar, public, private
+    // Değişken, nesne, fonksiyon vb. tüm özelliklere atanır. Sınıf hariç
+    // Her yerden erişim emvcut, uygulama duruna kadar yaşarlar.
+    // Staticler kullan yada kullanma yaşamaya devam ederler.
+    public static String name = "Ali Bilmem";
+
+    public static Random random = new Random();
+
+    //  sum static method
+    public static int sum( int a, int b ) {
+        return a + b;
+    }
 
 }
