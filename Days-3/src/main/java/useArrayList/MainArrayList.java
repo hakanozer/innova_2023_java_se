d2539862a672709f4b72536ab57b0114e99542d8
Hakan ÖZER <hakanozer02@gmail.com>
2023-05-31 15:06:56 +0300

    ArrayList - Property

--- Days-3/src/main/java/useArrayList/MainArrayList.java
+++ Days-3/src/main/java/useArrayList/MainArrayList.java
@@ -1,6 +1,7 @@
 package useArrayList;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class MainArrayList {
     public static void main(String[] args) {
@@ -79,6 +80,51 @@ public class MainArrayList {
         System.out.println(containsStatus);
 
         System.out.println( ls );
+        System.out.println("====================");
+
+        ArrayList<User> users = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            User u = new User();
+            u.setName("Name-"+i);
+            u.setSurname("Surname-"+i);
+            u.setPassword("p-"+i);
+            u.setEmail("mail@mail.com-"+i);
+            Random random = new Random();
+            u.setAge( random.nextInt(100) );
+            users.add(u);
+        }
+        System.out.println( users );
+
+        //users.removeIf( item -> item.getAge() < 17 );
+        //users.remove(5);
+
+        ArrayList<User> listFilter = new ArrayList<>();
+        for ( User item : users ) {
+            if ( item.getAge() > 17 ) {
+                listFilter.add(item);
+            }
+        }
+        users = listFilter;
+
+        for ( User item : users ) {
+            System.out.println(item);
+        }
+        System.out.println("====================");
+        ArrayList<String> lsx = new ArrayList<>();
+        lsx.add("İstanbul");
+        lsx.add("İstanbul");
+        lsx.add("İzmir");
+        lsx.add("İzmir");
+        lsx.add("İzmir");
+        lsx.add("Adana");
+        lsx.add("Adana");
+        lsx.add("Bursa");
+        lsx.add("Samsun");
+        lsx.add("Samsun");
+        lsx.add("Samsun");
+        lsx.add("Ankara");
+
+        System.out.println( lsx );
 
     }
 }
---
+++ Days-3/src/main/java/useArrayList/User.java
@@ -0,0 +1,14 @@
+package useArrayList;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+    private String name;
+    private String surname;
+    private String email;
+    private String password;
+    private int age;
+
+}
