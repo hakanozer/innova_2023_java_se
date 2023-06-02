3c4e5f527a00194be9ac2b48fb0d8ea4b60e9123
Hakan ÖZER <hakanozer02@gmail.com>
2023-06-02 15:22:02 +0300

    Xml

--- Days-5/src/main/java/MainApplication.java
+++ Days-5/src/main/java/MainApplication.java
@@ -1,5 +1,7 @@
+import models.Currency;
 import models.Note;
 import services.NoteService;
+import services.XmlService;
 
 import java.util.ArrayList;
 
@@ -8,20 +10,23 @@ public class MainApplication {
     public static void main(String[] args) {
 
         NoteService service = new NoteService();
-
         /*
         Note note = new Note();
         note.setTitle("Veli Toplantısı");
         note.setDetail("Toplantı Detay");
         int status = service.save(note);
         System.out.println("Status :" + status);
-         */
+
 
         ArrayList<Note> ls = service.allNote();
         for ( Note item : ls ) {
             System.out.println(item);
         }
+        */
 
+        XmlService xmlService = new XmlService();
+        ArrayList<Currency> ls = xmlService.xml();
+        System.out.println( ls.get(0) );
     }
 
 }
---
+++ Days-5/src/main/java/models/Currency.java
@@ -0,0 +1,14 @@
+package models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Currency {
+    private String currencyName;
+    private String forexBuying;
+    private String forexSelling;
+    private String banknoteBuying;
+    private String banknoteSelling;
+}
